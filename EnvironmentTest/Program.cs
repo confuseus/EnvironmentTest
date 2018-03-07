e681f742059345744d4dd7bcb55a493d434b0bb6
confuseus <fuse87@gmail.com>
2018-03-07 13:32:38 +0000

    Added a visual studio solution

---
+++ EnvironmentTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnvironmentTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
